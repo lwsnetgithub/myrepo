0fbdb24e00d990f7868853fa0ff164d8b81dd987
brain <lwsnetlwsnet@yahoo.ca>
2015-02-23 15:32:04 -0500

    recommit

--- gittest/src/com/brian/HelloWorld.java
+++ gittest/src/com/brian/HelloWorld.java
@@ -3,7 +3,7 @@ package com.brian;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("HelloWorld printed ammended");
 
 	}
 
